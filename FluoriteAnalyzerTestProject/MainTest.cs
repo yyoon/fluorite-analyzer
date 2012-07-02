71874b3adad55a4f82183e241b1c387a30b92a08
YoungSeok Yoon <YoungSeok Yoon@nullstein-vpc.local>
2012-07-02 17:19:46 +0900

    test project added

---
+++ FluoriteAnalyzerTestProject/MainTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FluoriteAnalyzerTestProject
+{
+    [TestClass]
+    public class MainTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
