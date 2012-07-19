7a63b8ab2dc97326b8e9b26ebf94d8676e4cb566
YoungSeok Yoon <youngseok@cs.cmu.edu>
2012-07-19 20:27:05 +0900

    Refactored TypoCorrectionDetector

--- FluoriteAnalyzer/PatternDetectors/TypoCorrectionDetector.cs
+++ FluoriteAnalyzer/PatternDetectors/TypoCorrectionDetector.cs
@@ -17,27 +17,90 @@ namespace FluoriteAnalyzer.PatternDetectors
             return _instance ?? (_instance = new TypoCorrectionDetector());
         }
 
+        private static readonly int TIME_THRESHOLD = 3000;  // millisecond
+
         public override IEnumerable<PatternInstance> DetectAsPatternInstances(ILogProvider logProvider)
         {
-            int timethreshold = 3000;
-
             List<Event> completeList = logProvider.LoggedEvents.ToList();
             List<DocumentChange> dcList = logProvider.LoggedEvents.OfType<DocumentChange>().ToList();
+
+            List<PatternInstance> detectedPatterns = new List<PatternInstance>();
 
             foreach (int i in Enumerable.Range(0, dcList.Count))
             {
                 DocumentChange dc = dcList[i];
 
                 // Insert - Delete - Insert+
-                if (dc is Insert && logProvider.CausedByInsertString(dc) && !string.IsNullOrWhiteSpace(((Insert)dc).Text)) 
-                {
-                    int offset = dc.Offset;
-                    int length = dc.Length;
+                DetectInsertDeleteInsertPattern(logProvider, dcList, detectedPatterns, i, dc);
 
-                    if (i + 1 < dcList.Count && dcList[i + 1] is Delete && (dcList[i + 1].Timestamp - dc.Timestamp) < timethreshold)
+                // Insert - Replace - Insert*
+                DetectInsertReplaceInsertPattern(logProvider, completeList, dcList, detectedPatterns, i, dc);
+            }
+
+            return detectedPatterns;
+        }
+
+        private static void DetectInsertDeleteInsertPattern(ILogProvider logProvider, List<DocumentChange> dcList, List<PatternInstance> detectedPatterns, int i, DocumentChange dc)
+        {
+            if (dc is Insert && logProvider.CausedByInsertString(dc) && !string.IsNullOrWhiteSpace(((Insert)dc).Text))
+            {
+                int offset = dc.Offset;
+                int length = dc.Length;
+
+                if (i + 1 < dcList.Count && dcList[i + 1] is Delete && (dcList[i + 1].Timestamp - dc.Timestamp) < TIME_THRESHOLD)
+                {
+                    int offset2 = dcList[i + 1].Offset;
+                    int length2 = dcList[i + 1].Length;
+
+                    if (offset <= offset2 && offset2 + length2 <= offset + length)
                     {
-                        int offset2 = dcList[i + 1].Offset;
-                        int length2 = dcList[i + 1].Length;
+                        if (i + 2 < dcList.Count && dcList[i + 2] is Insert)
+                        {
+                            int offset3 = dcList[i + 2].Offset;
+
+                            if (offset2 == offset3)
+                            {
+                                var result = new PatternInstance(
+                                    dc,
+                                    3,
+                                    "\"" + ((Insert)dcList[i]).Text + "\" - \"" +
+                                        ((Delete)dcList[i + 1]).Text + "\" + \"" +
+                                        ((Insert)dcList[i + 2]).Text + "\""
+                                    );
+
+                                detectedPatterns.Add(result);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void DetectInsertReplaceInsertPattern(ILogProvider logProvider, List<Event> completeList, List<DocumentChange> dcList, List<PatternInstance> detectedPatterns, int i, DocumentChange dc)
+        {
+            if (dc is Insert && logProvider.CausedByInsertString(dc) && !string.IsNullOrWhiteSpace(((Insert)dc).Text))
+            {
+                int offset = dc.Offset;
+                int length = dc.Length;
+
+                if (i + 1 < dcList.Count && dcList[i + 1] is Replace &&
+                    !logProvider.CausedByAutoIndent(dcList[i + 1]) &&
+                    (dcList[i + 1].Timestamp - dc.Timestamp) < TIME_THRESHOLD)
+                {
+                    Replace replace = (Replace)dcList[i + 1];
+
+                    // Check the prev/next command
+                    int indexOfReplace = completeList.IndexOf(replace);
+                    Debug.Assert(indexOfReplace >= 0);
+
+                    // If previous command is content assist, don't count it.
+                    // If next command is PasteCommand, don't count it.
+                    if (!(completeList[indexOfReplace - 1] is AssistCommand) &&
+                        !(completeList.Count > indexOfReplace + 1 && completeList[indexOfReplace + 1] is PasteCommand))
+                    {
+
+                        int offset2 = replace.Offset;
+                        int length2 = replace.Length;
 
                         if (offset <= offset2 && offset2 + length2 <= offset + length)
                         {
@@ -45,80 +108,31 @@ namespace FluoriteAnalyzer.PatternDetectors
                             {
                                 int offset3 = dcList[i + 2].Offset;
 
-                                if (offset2 == offset3)
+                                if (offset3 == offset2 + replace.InsertionLength)
                                 {
                                     var result = new PatternInstance(
                                         dc,
                                         3,
                                         "\"" + ((Insert)dcList[i]).Text + "\" - \"" +
-                                            ((Delete)dcList[i + 1]).Text + "\" + \"" +
+                                            replace.DeletedText + "\" + \"" +
+                                            replace.InsertedText +
                                             ((Insert)dcList[i + 2]).Text + "\""
                                         );
 
-                                    yield return result;
+                                    detectedPatterns.Add(result);
                                 }
                             }
-                        }
-                    }
-                }
-
-                // Insert - Replace - Insert*
-                if (dc is Insert && logProvider.CausedByInsertString(dc) && !string.IsNullOrWhiteSpace(((Insert)dc).Text)) 
-                {
-                    int offset = dc.Offset;
-                    int length = dc.Length;
-
-                    if (i + 1 < dcList.Count && dcList[i + 1] is Replace &&
-                        !logProvider.CausedByAutoIndent(dcList[i + 1]) &&
-                        (dcList[i + 1].Timestamp - dc.Timestamp) < timethreshold)
-                    {
-                        Replace replace = (Replace)dcList[i + 1];
-
-                        // Check the prev/next command
-                        int indexOfReplace = completeList.IndexOf(replace);
-                        Debug.Assert(indexOfReplace >= 0);
-
-                        // If previous command is content assist, don't count it.
-                        // If next command is PasteCommand, don't count it.
-                        if (!(completeList[indexOfReplace - 1] is AssistCommand) &&
-                            !(completeList.Count > indexOfReplace + 1 && completeList[indexOfReplace + 1] is PasteCommand))
-                        {
-
-                            int offset2 = replace.Offset;
-                            int length2 = replace.Length;
-
-                            if (offset <= offset2 && offset2 + length2 <= offset + length)
+                            else
                             {
-                                if (i + 2 < dcList.Count && dcList[i + 2] is Insert)
-                                {
-                                    int offset3 = dcList[i + 2].Offset;
+                                var result = new PatternInstance(
+                                    dc,
+                                    2,
+                                    "\"" + ((Insert)dcList[i]).Text + "\" - \"" +
+                                        ((Replace)dcList[i + 1]).DeletedText + "\" + \"" +
+                                        ((Replace)dcList[i + 1]).InsertedText + "\""
+                                    );
 
-                                    if (offset3 == offset2 + replace.InsertionLength)
-                                    {
-                                        var result = new PatternInstance(
-                                            dc,
-                                            3,
-                                            "\"" + ((Insert)dcList[i]).Text + "\" - \"" +
-                                                replace.DeletedText + "\" + \"" +
-                                                replace.InsertedText +
-                                                ((Insert)dcList[i + 2]).Text + "\""
-                                            );
-
-                                        yield return result;
-                                    }
-                                }
-                                else
-                                {
-                                    var result = new PatternInstance(
-                                        dc,
-                                        2,
-                                        "\"" + ((Insert)dcList[i]).Text + "\" - \"" +
-                                            ((Replace)dcList[i + 1]).DeletedText + "\" + \"" +
-                                            ((Replace)dcList[i + 1]).InsertedText + "\""
-                                        );
-
-                                    yield return result;
-                                }
+                                detectedPatterns.Add(result);
                             }
                         }
                     }
