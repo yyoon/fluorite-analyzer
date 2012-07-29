f39056f54e3d2372cd4a81fde39280425f7cbc03
YoungSeok Yoon <youngseok@cs.cmu.edu>
2012-07-29 22:33:26 +0900

    Use reflection to detect all the pattern detectors, and add them to combo box.

--- FluoriteAnalyzer/Analyses/Patterns.Designer.cs
+++ FluoriteAnalyzer/Analyses/Patterns.Designer.cs
@@ -28,28 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.buttonSearchFixTypos = new System.Windows.Forms.Button();
             this.listViewPatterns = new System.Windows.Forms.ListView();
             this.columnPatternID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnPatternLength = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnPatternTimeInVideo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnPatternInfo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.buttonSearchParameterTuning = new System.Windows.Forms.Button();
             this.labelCount = new System.Windows.Forms.Label();
-            this.buttonFindErrorRecoveries = new System.Windows.Forms.Button();
-            this.buttonFindMovePatterns = new System.Windows.Forms.Button();
+            this.buttonDetectPatterns = new System.Windows.Forms.Button();
+            this.comboDetectors = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
-            // 
-            // buttonSearchFixTypos
-            // 
-            this.buttonSearchFixTypos.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonSearchFixTypos.Location = new System.Drawing.Point(187, 475);
-            this.buttonSearchFixTypos.Name = "buttonSearchFixTypos";
-            this.buttonSearchFixTypos.Size = new System.Drawing.Size(198, 23);
-            this.buttonSearchFixTypos.TabIndex = 3;
-            this.buttonSearchFixTypos.Text = "Find Fix Typos Patterns";
-            this.buttonSearchFixTypos.UseVisualStyleBackColor = true;
-            this.buttonSearchFixTypos.Click += new System.EventHandler(this.buttonSearchFixTypos_Click);
             // 
             // listViewPatterns
             // 
@@ -65,7 +52,7 @@
             this.listViewPatterns.Location = new System.Drawing.Point(0, 0);
             this.listViewPatterns.Name = "listViewPatterns";
             this.listViewPatterns.Size = new System.Drawing.Size(999, 472);
-            this.listViewPatterns.TabIndex = 2;
+            this.listViewPatterns.TabIndex = 0;
             this.listViewPatterns.UseCompatibleStateImageBehavior = false;
             this.listViewPatterns.View = System.Windows.Forms.View.Details;
             this.listViewPatterns.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listViewPatterns_MouseDoubleClick);
@@ -90,17 +77,6 @@
             this.columnPatternInfo.Text = "Additional Information";
             this.columnPatternInfo.Width = 400;
             // 
-            // buttonSearchParameterTuning
-            // 
-            this.buttonSearchParameterTuning.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonSearchParameterTuning.Location = new System.Drawing.Point(391, 475);
-            this.buttonSearchParameterTuning.Name = "buttonSearchParameterTuning";
-            this.buttonSearchParameterTuning.Size = new System.Drawing.Size(198, 22);
-            this.buttonSearchParameterTuning.TabIndex = 4;
-            this.buttonSearchParameterTuning.Text = "Find Parameter Tuning Patterns";
-            this.buttonSearchParameterTuning.UseVisualStyleBackColor = true;
-            this.buttonSearchParameterTuning.Click += new System.EventHandler(this.buttonSearchParameterTuning_Click);
-            // 
             // labelCount
             // 
             this.labelCount.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
@@ -108,38 +84,35 @@
             this.labelCount.Location = new System.Drawing.Point(3, 483);
             this.labelCount.Name = "labelCount";
             this.labelCount.Size = new System.Drawing.Size(0, 12);
-            this.labelCount.TabIndex = 5;
+            this.labelCount.TabIndex = 3;
             // 
-            // buttonFindErrorRecoveries
+            // buttonDetectPatterns
             // 
-            this.buttonFindErrorRecoveries.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonFindErrorRecoveries.Location = new System.Drawing.Point(595, 475);
-            this.buttonFindErrorRecoveries.Name = "buttonFindErrorRecoveries";
-            this.buttonFindErrorRecoveries.Size = new System.Drawing.Size(198, 22);
-            this.buttonFindErrorRecoveries.TabIndex = 6;
-            this.buttonFindErrorRecoveries.Text = "Error Recoveries";
-            this.buttonFindErrorRecoveries.UseVisualStyleBackColor = true;
-            this.buttonFindErrorRecoveries.Click += new System.EventHandler(this.buttonFindErrorRecoveries_Click);
+            this.buttonDetectPatterns.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonDetectPatterns.Location = new System.Drawing.Point(799, 475);
+            this.buttonDetectPatterns.Name = "buttonDetectPatterns";
+            this.buttonDetectPatterns.Size = new System.Drawing.Size(200, 22);
+            this.buttonDetectPatterns.TabIndex = 2;
+            this.buttonDetectPatterns.Text = "Detect Patterns";
+            this.buttonDetectPatterns.UseVisualStyleBackColor = true;
+            this.buttonDetectPatterns.Click += new System.EventHandler(this.buttonDetectPatterns_Click);
             // 
-            // buttonFindMovePatterns
+            // comboDetectors
             // 
-            this.buttonFindMovePatterns.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonFindMovePatterns.Location = new System.Drawing.Point(799, 475);
-            this.buttonFindMovePatterns.Name = "buttonFindMovePatterns";
-            this.buttonFindMovePatterns.Size = new System.Drawing.Size(198, 22);
-            this.buttonFindMovePatterns.TabIndex = 7;
-            this.buttonFindMovePatterns.Text = "Move Patterns";
-            this.buttonFindMovePatterns.UseVisualStyleBackColor = true;
-            this.buttonFindMovePatterns.Click += new System.EventHandler(this.buttonFindMovePatterns_Click);
+            this.comboDetectors.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboDetectors.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboDetectors.FormattingEnabled = true;
+            this.comboDetectors.Location = new System.Drawing.Point(593, 477);
+            this.comboDetectors.Name = "comboDetectors";
+            this.comboDetectors.Size = new System.Drawing.Size(200, 20);
+            this.comboDetectors.TabIndex = 1;
             // 
             // Patterns
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.Controls.Add(this.buttonFindMovePatterns);
-            this.Controls.Add(this.buttonFindErrorRecoveries);
+            this.Controls.Add(this.comboDetectors);
+            this.Controls.Add(this.buttonDetectPatterns);
             this.Controls.Add(this.labelCount);
-            this.Controls.Add(this.buttonSearchParameterTuning);
-            this.Controls.Add(this.buttonSearchFixTypos);
             this.Controls.Add(this.listViewPatterns);
             this.Name = "Patterns";
             this.Size = new System.Drawing.Size(1000, 500);
@@ -150,15 +123,13 @@
 
         #endregion
 
-        private System.Windows.Forms.Button buttonSearchFixTypos;
         private System.Windows.Forms.ListView listViewPatterns;
         private System.Windows.Forms.ColumnHeader columnPatternID;
         private System.Windows.Forms.ColumnHeader columnPatternLength;
         private System.Windows.Forms.ColumnHeader columnPatternTimeInVideo;
         private System.Windows.Forms.ColumnHeader columnPatternInfo;
-        private System.Windows.Forms.Button buttonSearchParameterTuning;
         private System.Windows.Forms.Label labelCount;
-        private System.Windows.Forms.Button buttonFindErrorRecoveries;
-        private System.Windows.Forms.Button buttonFindMovePatterns;
+        private System.Windows.Forms.Button buttonDetectPatterns;
+        private System.Windows.Forms.ComboBox comboDetectors;
     }
 }
